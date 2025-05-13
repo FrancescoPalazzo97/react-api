653b2c4aa068cf89fff39da9e7c0ccda3e3f50d4
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-13 16:02:00 +0200

    [UPD] 3rd  milestone done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const ActressesAPI = "https://lanciweb.github.io/demo/api/actresses/";
 const ActorsAPI = "https://lanciweb.github.io/demo/api/actors/";
@@ -28,26 +30,8 @@ function App() {
   return (
     <>
       <div className="container">
-        <header className="row">
-          <div className="col">
-            <h1 className="text-center">React API</h1>
-          </div>
-        </header>
-        <main className="row gy-4">
-          <div className="col-lg-3 col-m-4 col-sm-6">
-            <div className="card">
-              <img src={actresses[0].image} className="card-img-top" alt="Actress 1" />
-              <div className="card-body">
-                <h5 className="card-title">{actresses[0].name}</h5>
-                <p className="card-text">{actresses[0].biography}</p>
-                <p className="card-text">{actresses[0].birth_year}</p>
-                <p className="card-text">{actresses[0].nationality}</p>
-                <p className="card-text">{actresses[0].awards}</p>
-              </div>
-            </div>
-          </div>
-
-        </main>
+        <Header />
+        <Main actresses={actresses} />
       </div>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="row my-5">
+            <div className="col">
+                <h1 className="text-center">React API</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
