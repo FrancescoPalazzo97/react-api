e4ace377d3513ee072448056c186b36b73c497b2
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-13 15:24:46 +0200

    [UPD] 1st milestone done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import axios from "axios";
+import { useState, useEffect, use } from "react";
+
 const ActressesAPI = "https://lanciweb.github.io/demo/api/actresses/";
 const ActorsAPI = "https://lanciweb.github.io/demo/api/actors/";
 
 function App() {
 
+  const fetchActresses = () => {
+    axios.get(ActressesAPI)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching actresses:", error);
+      });
+  }
+
+  useEffect(() => {
+    fetchActresses();
+  }
+    , []);
+
   return (
     <>
+      <div className="container">
+        <header className="row">
+          <div className="col">
+            <h1 className="text-center">React API</h1>
+          </div>
+        </header>
+        <main className="row">
 
+        </main>
+      </div>
     </>
   )
 }
