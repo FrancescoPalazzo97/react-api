6668880a0e0b9fa35d1f1267a6ebe9f355217b1a
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-13 17:38:37 +0200

    [UPD] creazione nuovo array

--- src/App.jsx
+++ src/App.jsx
@@ -56,14 +56,64 @@ function App() {
     , []);
 
   useEffect(() => {
-    gender === `male`
-      ? setShow(actors)
-      : gender === `female`
-        ? setShow(actresses)
-        : setShow([...actors, ...actresses])
-  }, [gender, actors, actresses]);
+    let filtered = [];
+    if (gender === `male`) {
+      filtered = actors.map(actor => ({
+        id: actor.id,
+        name: actor.name,
+        image: actor.image,
+        biography: actor.biography,
+        birth_year: actor.birth_year,
+        nationality: actor.nationality,
+        awards: actor.awards,
+      }))
+    } else if (gender === `female`) {
+      filtered = actresses.map(actress => ({
+        id: actress.id,
+        name: actress.name,
+        image: actress.image,
+        biography: actress.biography,
+        birth_year: actress.birth_year,
+        nationality: actress.nationality,
+        awards: actress.awards,
+      }))
+    } else {
+      let maxLenght = actresses.length + actors.length
+      for (let i = 0; i < maxLenght; i++) {
+        filtered.push({
+          id: i + 1,
+          name: actors[i].name,
+          image: actors[i].image,
+          biography: actors[i].biography,
+          birth_year: actors[i].birth_year,
+          nationality: actors[i].nationality,
+          awards: actors[i].awards,
+        })
+        filtered.push({
+          id: i + 2,
+          name: actresses[i].name,
+          image: actresses[i].image,
+          biography: actresses[i].biography,
+          birth_year: actresses[i].birth_year,
+          nationality: actresses[i].nationality,
+          awards: actresses[i].awards,
+        })
+      }
+    }
 
-  console.log(gender)
+    setShow(filtered)
+
+  }, [gender, actors, actresses])
+
+  // useEffect(() => {
+  //   gender === `male`
+  //     ? setShow(actors)
+  //     : gender === `female`
+  //       ? setShow(actresses)
+  //       : setShow([...actors, ...actresses])
+  // }, [gender, actors, actresses]);
+
+  console.log(actors[0])
 
 
   return (
@@ -73,13 +123,16 @@ function App() {
         <main>
           <section className="row">
             <div className="col my-2">
+              <button className="btn btn-primary me-2" onClick={setAll}>
+                Tutti
+              </button>
               <button className="btn btn-primary me-2" onClick={setMale}>
                 Attori
               </button>
               <button className="btn btn-primary" onClick={setFemale}>
                 Attrici
-              </button
-              ></div>
+              </button>
+            </div>
           </section>
           <CardSection show={show} />
         </main>
