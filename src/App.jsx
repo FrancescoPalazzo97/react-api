c38047e3f15d3d462452a378385841952fad5fb7
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-13 15:44:16 +0200

    [UPD] 2nd milestone done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 
 const ActressesAPI = "https://lanciweb.github.io/demo/api/actresses/";
 const ActorsAPI = "https://lanciweb.github.io/demo/api/actors/";
 
 function App() {
 
+  const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
+
   const fetchActresses = () => {
     axios.get(ActressesAPI)
       .then((res) => {
         console.log(res.data);
+        setActresses(res.data);
       })
       .catch((error) => {
         console.error("Error fetching actresses:", error);
@@ -29,7 +33,19 @@ function App() {
             <h1 className="text-center">React API</h1>
           </div>
         </header>
-        <main className="row">
+        <main className="row gy-4">
+          <div className="col-lg-3 col-m-4 col-sm-6">
+            <div className="card">
+              <img src={actresses[0].image} className="card-img-top" alt="Actress 1" />
+              <div className="card-body">
+                <h5 className="card-title">{actresses[0].name}</h5>
+                <p className="card-text">{actresses[0].biography}</p>
+                <p className="card-text">{actresses[0].birth_year}</p>
+                <p className="card-text">{actresses[0].nationality}</p>
+                <p className="card-text">{actresses[0].awards}</p>
+              </div>
+            </div>
+          </div>
 
         </main>
       </div>
